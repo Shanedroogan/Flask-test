4bbc42b1302d2f75454155bec256cd170bb19d95
shanedroogan <shanedroogan@gmail.com>
2020-03-16 17:49:44 -0400

    Added Followers and Pagination functionality

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    POSTS_PER_PAGE = 3
+
     #MAIL SERVER INFO
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
--- venv/Scripts/alembic-script.py
+++ venv/Scripts/alembic-script.py
@@ -1,6 +1,6 @@
 #!c:\users\shane\flask-test\venv\scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==1.3.2','console_scripts','alembic'
-__requires__ = 'alembic==1.3.2'
+# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==1.4.1','console_scripts','alembic'
+__requires__ = 'alembic==1.4.1'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('alembic==1.3.2', 'console_scripts', 'alembic')()
+        load_entry_point('alembic==1.4.1', 'console_scripts', 'alembic')()
     )
